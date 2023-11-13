c70fa4d5c5fb8e365314913c5910fb0e23797f6e
colzani_andrea <andreacolzani.05@gmail.com>
2023-11-13 12:29:06 +0100

    aggiunta codice per connessione tcp

--- Client/Duel2D/Screen.cs
+++ Client/Duel2D/Screen.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using System.Reflection.Metadata;
+using System.Net.Sockets;
 
 namespace Duel2D
 {
@@ -15,13 +16,14 @@ namespace Duel2D
         public Texture2D sMenu { get; set; }
         public Texture2D sNome { get; set; }
         public SpriteFont fAll { get; set; }
+        public tcpClass clientTcp { get; set; }
 
         private float opacitaTI = 1f;
         private bool sensoOpacita = true;
 
         public Screen()
         {
-            
+            clientTcp = new tcpClass("172.16.102.109", 666);
         }
 
         public void carica(Microsoft.Xna.Framework.Content.ContentManager content)
@@ -52,7 +54,7 @@ namespace Duel2D
             spriteBatch.End();
         }
 
-        public void DrawStart(SpriteBatch spriteBatch)
+        public void animazioneTesto()
         {
             if (sensoOpacita)
             {
@@ -72,6 +74,12 @@ namespace Duel2D
                     sensoOpacita = true;
                 }
             }
+        }
+
+        public void DrawStart(SpriteBatch spriteBatch)
+        {
+            animazioneTesto();
+
 
             spriteBatch.Begin();
             spriteBatch.Draw(sIniziale, new Rectangle(0, 0, 1200, 800), Color.White);
@@ -81,8 +89,14 @@ namespace Duel2D
 
         public void DrawMenu(SpriteBatch spriteBatch)
         {
+            clientTcp.connettiServer();
+            clientTcp.inviaMessaggio("Hello, server!");
+
+
+
             spriteBatch.Begin();
             spriteBatch.Draw(sMenu, new Rectangle(0, 0, 1200, 800), Color.White);
+            spriteBatch.Draw(sMenu, new Rectangle(10, 10, 80, 35), Color.White);
             spriteBatch.Draw(sNome, new Rectangle(500, 100, 190, 80), Color.White);
             spriteBatch.End();
         }
---
+++ Client/Duel2D/tcpClass.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Duel2D
+{
+    public class tcpClass
+    {
+        private static TcpClient client;
+        private static NetworkStream stream;
+        private static bool isRunning = true;
+        private string ipAdress;
+        private int port;
+        private string msg = "ciao";
+
+        public tcpClass(string ipAddress, int port)
+        {
+            this.ipAdress = ipAddress;
+            this.port = port;
+        }
+
+        public void connettiServer()
+        {
+            client = new TcpClient(ipAdress, port);
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(msg);
+            stream = client.GetStream();
+
+            //stream.Write(data, 0, data.Length);           INVIA
+            //Console.WriteLine("Sent: {0}", msg);
+
+
+            data = new Byte[256];                           //RICEVI
+            String responseData = String.Empty;
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine("Received: {0}", responseData);
+
+            /*
+            Thread receiveThread = new Thread(riceviMessaggio);
+            receiveThread.Start();
+
+
+            // Invia un messaggio al server
+            Byte[] data2 = System.Text.Encoding.ASCII.GetBytes(msg);
+            stream.Write(data2, 0, data2.Length);         
+            Console.WriteLine("Sent: {0}", msg);
+            */
+
+
+
+
+
+
+
+
+            /*
+             * stream.Close();
+            client.Close();
+             * 
+             * 
+            try
+            {
+                client = new TcpClient(ipAdress, port);
+                stream = client.GetStream();
+                Console.WriteLine("Connected to server.");
+
+                // Avvia il thread per la ricezione dei messaggi in modo asincrono
+                Thread receiveThread = new Thread(riceviMessaggio);
+                receiveThread.Start();
+
+                // Invia un messaggio al server
+                inviaMessaggio("Hello, server!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e);
+            }
+            */
+        }
+
+        public void riceviMessaggio()
+        {
+            while (isRunning)
+            {
+                try
+                {
+                    byte[] receivedBytes = new byte[1024];
+                    int byteCount = stream.Read(receivedBytes, 0, receivedBytes.Length);
+
+                    if (byteCount > 0)
+                    {
+                        string receivedMessage = Encoding.ASCII.GetString(receivedBytes, 0, byteCount);
+                        Console.WriteLine("Server: " + receivedMessage);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e);
+                    isRunning = false;
+                }
+            }
+        }
+
+        public void inviaMessaggio(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e);
+            }
+        }
+
+        /*
+        static void Main()
+        {
+            string serverIP = "127.0.0.1"; // Indirizzo IP del server
+            int serverPort = 8080; // Porta del server
+
+            connettiServer();
+
+            // Esempio di invio di un messaggio al server
+            while (isRunning)
+            {
+                string message = Console.ReadLine();
+                SendMessage(message);
+            }
+
+            stream.Close();
+            client.Close();
+        }
+        */
+    }
+}
