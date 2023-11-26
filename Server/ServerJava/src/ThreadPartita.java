a8675b2dc80f22e595aa21affca2fc6134927b9d
Emiliano Padula <padulaemiliano05@gmail.com>
2023-11-26 22:52:56 +0100

    Sistemati codici di comunicazione server

--- Server/ServerJava/src/ThreadPartita.java
+++ Server/ServerJava/src/ThreadPartita.java
@@ -23,39 +23,55 @@ public class ThreadPartita extends Thread {
             try {
                 msg = connessione.ricevi(nClientPrincipale);
                 System.out.println(msg);
-                if (msg.contains("colpito")) {
-                    int vita = giocatorePrincipale.colpito();
-                    if (vita > 0) {
-                        connessione.invia(giocatorePrincipale.nome + ";" + vita, nClientPrincipale);
-                        connessione.invia(giocatorePrincipale.nome + ";" + vita, nClientSecondario);
-                    } else {
-                        connessione.invia(giocatorePrincipale.nome + ";" + "sconfitta", nClientPrincipale);
-                        connessione.invia(giocatorePrincipale.nome + ";" + "vittoria", nClientSecondario);
-                    }
 
-                } else if (msg.contains("muovi")) {
-                    Messaggio messaggio = new Messaggio(msg);
-                    messaggio.Splitta();
-                    giocatorePrincipale.muovi(Integer.parseInt(messaggio.x), Integer.parseInt(messaggio.y));
-                    connessione.invia(messaggio.toCsv(), nClientPrincipale);
-                    connessione.invia(messaggio.toCsv(), nClientSecondario);
-                } else if (msg.contains("salta")) {
-                    connessione.invia(giocatorePrincipale.nome + ";" + "salta", nClientPrincipale);
-                    connessione.invia(giocatorePrincipale.nome + ";" + "salta", nClientSecondario);
-                } else if (msg.contains("fine")) {
-                    connessione.invia("fine", nClientPrincipale);
-                    connessione.invia("fine", nClientSecondario);
-                    inCorso = false;
-                } else if (msg.contains("rivincita")) {
-                    giocatorePrincipale.vita = 100;
-                    giocatoreSecondario.vita = 100;
-                } else if (msg.contains("sparaADestra")) {
-                    connessione.invia(giocatorePrincipale.nome + ";" + "sparaADestra", nClientPrincipale);
-                    connessione.invia(giocatorePrincipale.nome + ";" + "sparaADestra", nClientSecondario);
-                } else if (msg.contains("sparaASinistra")) {
-                    connessione.invia(giocatorePrincipale.nome + ";" + "sparaASinistra", nClientPrincipale);
-                    connessione.invia(giocatorePrincipale.nome + ";" + "sparaASinistra", nClientSecondario);
-                }
+                Messaggio messaggio = new Messaggio(msg);
+                connessione.invia(messaggio.toCsv(), nClientPrincipale);
+                connessione.invia(messaggio.toCsv(), nClientSecondario);
+
+                /*
+                 * if (msg.contains("colpito")) {
+                 * int vita = giocatorePrincipale.colpito();
+                 * if (vita > 0) {
+                 * connessione.invia(giocatorePrincipale.nome + ";" + vita, nClientPrincipale);
+                 * connessione.invia(giocatorePrincipale.nome + ";" + vita, nClientSecondario);
+                 * } else {
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "sconfitta",
+                 * nClientPrincipale);
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "vittoria",
+                 * nClientSecondario);
+                 * }
+                 * 
+                 * } else if (msg.contains("muovi")) {
+                 * Messaggio messaggio = new Messaggio(msg);
+                 * messaggio.Splitta();
+                 * giocatorePrincipale.muovi(Integer.parseInt(messaggio.x),
+                 * Integer.parseInt(messaggio.y));
+                 * connessione.invia(messaggio.toCsv(), nClientPrincipale);
+                 * connessione.invia(messaggio.toCsv(), nClientSecondario);
+                 * } else if (msg.contains("salta")) {
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "salta",
+                 * nClientPrincipale);
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "salta",
+                 * nClientSecondario);
+                 * } else if (msg.contains("fine")) {
+                 * connessione.invia("fine", nClientPrincipale);
+                 * connessione.invia("fine", nClientSecondario);
+                 * inCorso = false;
+                 * } else if (msg.contains("rivincita")) {
+                 * giocatorePrincipale.vita = 100;
+                 * giocatoreSecondario.vita = 100;
+                 * } else if (msg.contains("sparaADestra")) {
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "sparaADestra",
+                 * nClientPrincipale);
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "sparaADestra",
+                 * nClientSecondario);
+                 * } else if (msg.contains("sparaASinistra")) {
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "sparaASinistra",
+                 * nClientPrincipale);
+                 * connessione.invia(giocatorePrincipale.nome + ";" + "sparaASinistra",
+                 * nClientSecondario);
+                 * }
+                 */
 
             } catch (IOException e) {
                 e.printStackTrace();
